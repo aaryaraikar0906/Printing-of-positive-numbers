827ef707ae096bfaa5892b237a210c55281913a8
aaryaraikar0906 <63286348+aaryaraikar0906@users.noreply.github.com>
2020-05-09 00:05:47 +0530

    Add files via upload

---
+++ printing of positive numbers.py
@@ -0,0 +1,20 @@
+# PART 1
+list = [12,-7,5,64,-14]
+print("Input: List1 ",list)
+print("Positive numbers out of the given list is: ")
+#For loop
+for x in list:
+    #if loop : condition for positive numbers
+    if x>=0:
+        print(x,end = " ")
+
+print(" ")
+# PART 2
+list = [12,14,-95,3]
+print("Input: List2 ",list)
+print("Positive numbers out of the given list is: ")
+#For loop
+for x in list:
+    #if loop : condition for positive numbers
+    if x>=0:
+        print(x,end = " ")
